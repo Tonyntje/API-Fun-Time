7a6a10e917fb55a39a26f1cfbfc17b2b47f37868
Tony Nieuwenhuijs <tony_nieuwenhuijs@live.nl>
2022-05-01 21:52:00 +0200

    Updated to with updated and smarter code. Also a randomizer function added for fun :-)

--- main.js
+++ main.js
@@ -17,18 +17,24 @@ async function getPokeDetails() {
 async function getDetails(pokemonUrl) {
     await fetch(pokemonUrl, {method: "GET"})
     .then(respons => respons.json())
-    .then(res => allPokemons
-        // Hard to read, so multiline it is
-        .push( 
-            { 
-            name:       res.name, 
-            image:      res.sprites.back_default, 
-            hp:         res.stats[0].base_stat, 
-            attack:     res.stats[1].base_stat, 
-            defense:    res.stats[2].base_stat
-            }
-        )
-    ).then(() => {
+    .then(res => { allPokemons
+            // Hard to read, so multiline it is
+            .push( 
+                { 
+                name:       res.name, 
+                image:      res.sprites.front_default, 
+                hp:         res.stats[0].base_stat, 
+                attack:     res.stats[1].base_stat, 
+                defense:    res.stats[2].base_stat
+                }
+            )
+            const hidden = document.getElementById('store-images')
+            const temp = document.createElement('img')
+            temp.src = res.sprites.front_default, 
+            hidden.append(temp)
+        }
+    )
+    .then(() => {
         const button = document.getElementById('buttonlistener');
         button.classList.remove('loading')
         button.addEventListener('click', whoIsMyPokemon);
@@ -39,9 +45,26 @@ getPokeDetails();
 
 // On Button press get a random pokemon.
 const whoIsMyPokemon = () => {
-    const pokemon = allPokemons[Math.round(Math.random() * 151)]
-    console.log(pokemon);
-
+    // Creating a card design for a pokemon
+    const container = document.getElementById('pokecontainer')
+    const amount = 20;
+    for(let c = 0; c < amount; c++) {
+        setTimeout(() => {
+            const pokemon = allPokemons[Math.round(Math.random() * 151)]
+            container.innerHTML = `
+            <img class="random" src="${pokemon.image}">
+            <h3>${pokemon.name}</h3>
+            <div class="stats">
+                <div class="hp">${pokemon.hp}</div>
+                <div class="attack">${pokemon.attack}</div>
+                <div class="defense">${pokemon.defense}</div>
+            </div>`
+        }, 100*c);
+    }
+    setTimeout(() => {
+        const last = document.querySelector('.random');
+        last.classList.remove('random')
+    }, 100*amount)
 }
 
 
